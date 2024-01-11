eed82a2ec5f0435ec65bd1005344fb6609c857bb
Priyanshu Kumar choubey <104070944+PriyanshuChoubey@users.noreply.github.com>
2024-01-11 08:22:29 +0000

    conversionOf Data-types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let num = undefined
+
+let stringNum = String(num)
+
+console.log(typeof stringNum);
+console.log(stringNum);
+
+/************************************************* */
+let userLogedIn = undefined
+
+let booleanResult = Boolean(userLogedIn)
+
+console.log(typeof booleanResult);
+console.log(booleanResult);
+
+/*
+    1 --> true
+    0 --> false
+    "" --> false
+    "something" -->true
+    null --> false
+    undefined --> false
+*/
+
+/****************************************************** */
+let stringEx = false
+
+let inNumber = Number(stringEx)
+
+console.log(typeof inNumber);
+console.log(inNumber);
+
+/*
+    "33"  --> 33
+    ""    --> 0
+    "33abc" --> NaN  (Not a Number)
+    "abc"  --> NaN
+    null -->0
+    undefined --> NaN
+    true  --> 1
+    false --> 0
+*/
